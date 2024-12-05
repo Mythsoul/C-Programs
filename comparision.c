23c8d1168f134fca21424cebccc04faacf87a584
Mythsoul <Mythsoul2023@gmail.com>
2024-12-05 21:53:22 +0545

    comparision

---
+++ comparision.c
@@ -0,0 +1,101 @@
+#include<stdio.h> 
+
+// int main(){ 
+//  int a , b , c; 
+//  printf("Enter three numbers");
+//  scanf("%d %d %d" , &a , &b , &c); 
+//  if(a >b && a>c ){ 
+//     printf("The number %d is Greatest Among all" , a);
+//   }else if(b >a && b>c){ 
+//     printf("The number %d is Greatest Among all" , b);
+//   }else{ 
+//     printf("The number %d is Greatest Among all" , c);
+//   }
+// return 0;
+//   }
+
+
+// Smallest One among Three numbers 
+ 
+// int main(){ 
+//   int a , b , c; 
+//   printf("Enter three numbers");
+//   scanf("%d %d %d" , &a , &b , &c);
+//   if(a <b && a<c){ 
+//     printf("The number %d is Smallest Among all" , a);
+//   }else if(b <a && b<c){
+//     printf("The number %d is Smallest Among all" , b);
+//   }else if(c <a && c<b){
+//     printf("The number %d is Smallest Among all" , c);
+//   } else{
+//     printf("All numbers are equal");
+//   }
+// }
+
+
+
+// Check weather a number is positive or negative or zero
+
+// int main(){ 
+//     int num; 
+//     printf("Enter any number");
+//     scanf("%d" , &num);
+
+//     if(num > 0){ 
+//         printf("The number is positive");
+//     }else if(num < 0){ 
+//         printf("The number is Negative");
+//     }else{ 
+//         printf("The number is Zero");
+//     }
+//     return 0;
+// }
+
+// Check weather a number is odd or even
+
+// int main(){ 
+//   int a ; 
+//   printf("enter any number "); 
+//   scanf("%d" , &a); 
+
+// if(a % 2 == 0){
+//     printf("The number is Even");
+// }else{
+//     printf("The number is Odd");
+// }
+// return 0;
+//}
+
+
+// Check weather a number is divisible by 3 or not
+
+// int main(){ 
+//   int a ; 
+//   printf("Enter any number "); 
+// //   scanf("%d" , &a);
+
+// //   if(a % 3 == 0){ 
+// //       printf("The number is divisible by 3");
+// //   }else{
+// //       printf("The number is not divisible by 3");
+// //   }
+// //   return 0;
+// // }
+
+
+
+// // Check weather a number is divisible by 5 or not
+
+// int main(){ 
+//     int a ; 
+//     printf("Enter any number "); 
+//     scanf("%d" , &a);
+
+//     if(a % 5 == 0){ 
+//         printf("The number is divisible by 5");
+//     }else{
+//         printf("The number is not divisible by 5");
+//     }
+//     return 0;
+// }
+
