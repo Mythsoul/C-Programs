7227cc36d7db028d8119b85234613a137a8d7089
Mythsoul <Mythsoul2023@gmail.com>
2024-12-05 21:42:04 +0545

    add

---
+++ secondProgram.c
@@ -0,0 +1,81 @@
+#include<stdio.h> 
+// Sum of Two numbers 
+// int main(){ 
+//      int a,b , sum; 
+//      printf("Enter two numbers");
+//      scanf("%d %d" , &a , &b);
+//      sum = a + b;
+//      printf("The Sum is %d" , sum);
+//      return 0;
+// }
+
+// Sum of Two number another 
+
+// int main(){
+//      int a ; 
+//      float b , sum;
+//      printf("Enter two Numbers to sum : ");
+//      scanf("%d %f" , &a , &b);
+//      sum = a + b;
+//      printf("The Sum is %f" , sum);
+//      return 0;
+// }
+
+// Sum of three numbers 
+
+// int main(){
+//      int a , b , c , sum;
+//      printf("Enter three numbers to sum : ");
+//      scanf("%d %d %d" , &a , &b , &c);
+//      sum = a + b + c;
+//      printf("The Sum is %d" , sum);
+//      return 0;
+// }
+
+// Sum of two float number 
+
+// int main(){
+//      float a , b , sum;
+//      printf("Enter two float numbers to sum : ");
+//      scanf("%f %f" , &a , &b);
+//      sum = a + b;
+//      printf("The Sum is %f" , sum);
+//      return 0;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
